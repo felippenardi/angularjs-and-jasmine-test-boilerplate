552fc2aa5442012d88c42381b0def42979c52699
Felippe Nardi <felippe@nardi.me>
2015-04-15 15:11:14 -0400

    Add basic template for controller and service

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var myApp = angular.module('myApp',[]);
+
+
+myApp.factory('myService', function () {
+  var service = {};
+
+  return service;
+});
+
+myApp.controller('MainCtrl', function (myService) {
+  var controller = {};
+
+  return controller;;
+});
